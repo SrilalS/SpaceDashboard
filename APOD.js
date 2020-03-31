0d94152faed1038d42dea36bfd624b7d4364d7c3
Srilal Sachintha <37685396+SrilalS@users.noreply.github.com>
2020-03-31 19:47:12 +0530

    APOD

---
+++ APOD.js
@@ -0,0 +1,24 @@
+var settings = {
+    "url": "https://api.nasa.gov/planetary/apod?api_key=kjPLe9RNsz0X0qWhuMutlMH3V3S15oOZFsKskTSH",
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+var imgurl;
+var desc;
+var imghdurl;
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    imghdurl = response['hdurl'];
+    imgurl = response['url'];
+    desc = response['explanation'];
+    console.log(imghdurl);
+    console.log(imgurl);
+    console.log(desc);
+
+    document.getElementById('image').src = imgurl;
+    document.getElementById('desc').innerText = desc;
+    //document.getElementById('newcases').innerHTML = nc;
+    //document.getElementById('deaths').innerHTML = td;
+});
